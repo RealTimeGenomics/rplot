de8fd317cf3ccab12cd349a814295073c77fe16b
Len Trigg <len@netvalue.net.nz>
2005-11-29 21:24:48 +0000

    Moved config file here.

---
+++ .cvscheck/webrun_rplot.sh
@@ -0,0 +1,39 @@
+#! /bin/sh
+
+LOGNAME=${LOGNAME:-$USER}
+if [ ! "$HOME" ]; then
+    export HOME=/home/$LOGNAME
+fi
+if [ ! "$CVSROOT" ]; then
+    export CVSROOT=:pserver:$LOGNAME@giger:/home/cvs-repository2
+fi
+export JAVA=/usr/local/java/jdk
+export PATH=$PATH:$JAVA/bin:$HOME/bin:/usr/local/bin
+export SYSNAME=cvscheck_rplot
+
+if [ "$LOGNAME" == "syscheck" ]; then
+    export WEBROOT=/share/reeltwo/html/keachecker
+    export SCRIPTS=$WEBROOT/cvscheck
+    export CODEHOME=$WEBROOT/${SYSNAME}_java
+    export WEB=$WEBROOT/html/${SYSNAME}
+    export EMAIL_NOTIFY=1
+else
+    export CODEHOME=$HOME/reeltwo_sandboxes
+fi
+
+export MAIN_MODULE="rplot"
+export SRC_MODULES="rplot/src"
+export LIB_MODULES="rplot/lib"
+export TEST_MODULES="rplot/test"
+export DOC_MODULES=" "
+export JAVADOC_MODULES="rplot/src"
+export JIKES_OPTS="+E +P -source 1.4 -deprecation"
+export LEVEL=2
+
+
+(
+    (sh $SCRIPTS/cvscheck.sh cvscheck_main 2>&1)
+) >/dev/null
+
+
+
